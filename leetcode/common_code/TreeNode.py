f6269a148884b7493b85b8b296c4998156dff14d
Gyanesh Mittal <gyanesh.cse@gmail.com>
2019-02-23 13:58:06 -0800

    leetCode questions

---
+++ leetcode/common_code/TreeNode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
