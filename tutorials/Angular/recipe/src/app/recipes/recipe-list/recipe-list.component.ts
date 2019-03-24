55689ceac1f1e252eeffe5e6f7ca0d181071c8cf
Gyanesh Mittal <gyanesh.cse@gmail.com>
2019-03-24 10:51:38 -0700

    @angular_recipe Recipe Component and model

--- tutorials/Angular/recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ tutorials/Angular/recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Simply a test', 'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'),
+    new Recipe('A test recipe', 'Simply a test', 'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d')
+  ];
 
   constructor() { }
 
