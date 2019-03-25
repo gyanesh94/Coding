469cf1fa2baf4f7031ffe0a7bde7e12d78eade3c
Gyanesh Mittal <gyanesh.cse@gmail.com>
2019-03-25 01:18:37 -0700

    @angular_basics list events on custom directive using HostListener 3rd method of binding element properties to custom directive using HostBinding Directive Property binding to take values from developer to custom directive

--- tutorials/Angular/basics/src/app/directives/better-highlight.directive.ts
+++ tutorials/Angular/basics/src/app/directives/better-highlight.directive.ts
@@ -1,14 +1,29 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'transparent';
+  // 3rd Methed to use the directive
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
   }
 
 }
