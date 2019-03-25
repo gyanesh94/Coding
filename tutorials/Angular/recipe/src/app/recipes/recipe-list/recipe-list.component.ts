165042024a5e9c211fff4d1a8a315b3070154260
Gyanesh Mittal <gyanesh.cse@gmail.com>
2019-03-25 00:02:52 -0700

    @angular_recipes impleted header long way using ngIf and buttons implemented Recipes List and detail component using Property Binding and event emittion

--- tutorials/Angular/recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ tutorials/Angular/recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,28 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Simply a test', 'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'),
-    new Recipe('A test recipe', 'Simply a test', 'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d')
+    new Recipe(
+      'A recipe',
+      'Simply a test',
+      'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'
+      ),
+    new Recipe(
+      'A test recipe',
+      'Simply test',
+      'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'
+      )
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
