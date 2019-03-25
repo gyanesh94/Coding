3a11066f3582f3fda26aa96b99c1679a1dfaae96
Gyanesh Mittal <gyanesh.cse@gmail.com>
2019-03-25 13:45:53 -0700

    @angular_recipe corrected the tab indentation to size 2

--- tutorials/Angular/recipe/src/app/recipes/recipe.service.ts
+++ tutorials/Angular/recipe/src/app/recipes/recipe.service.ts
@@ -3,22 +3,22 @@ import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A recipe',
-            'Simply a test',
-            'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'
-        ),
-        new Recipe(
-            'A test recipe',
-            'Simply test',
-            'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'
-        )
-    ];
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A recipe',
+      'Simply a test',
+      'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'
+    ),
+    new Recipe(
+      'A test recipe',
+      'Simply test',
+      'https://c.pxhere.com/photos/b9/d3/cookbook_recipes_food_cook_book_paper_hand_culinary-881861.jpg!d'
+    )
+  ];
 
-    getRecipes() {
-        return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
--- tutorials/Angular/recipe/src/app/shopping-list/shopping-list.service.ts
+++ tutorials/Angular/recipe/src/app/shopping-list/shopping-list.service.ts
@@ -2,19 +2,19 @@ import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
-    private ingredients: Ingredient[] = [
-        new Ingredient('Apple', 5),
-        new Ingredient('Tomatoes', 10)
-    ];
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
-    getIngredients() {
-        return this.ingredients.slice();
-    }
+  getIngredients() {
+    return this.ingredients.slice();
+  }
 
-    addIngredients(ingredient: Ingredient) {
-        this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
-    }
+  addIngredients(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 
 }
